81d549640db037feafb78c9496bae205e4a368b5
Felipe Campos <felipecampos50123@gmail.com>
2024-06-14 10:29:09 -0300

    Criando script bash para executar primeira e segunda etapa do Meltano dentro do ambiente virtual do Apache Airflow

---
+++ run_first_step.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ./first_step/venv/bin/activate
+cd ./first_step
+python script.py
---
+++ run_second_step.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ./second_step/venv/bin/activate
+cd ./second_step
+python script.py
